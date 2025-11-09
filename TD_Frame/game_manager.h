5e177194ebbf86535afebb7d71e65daf25950814
Chr11111s <2292312771@qq.com>
2025-11-09 20:47:54 +0800

    可继承的单例模板类实现
    可继承的单例模板类实现

---
+++ TD_Frame/game_manager.h
@@ -0,0 +1,17 @@
+#ifndef _GAME_MANAGER_H_
+#define _GAME_MANAGER_H_
+
+#include "manager.h"
+
+class GameManager : public Manager<GameManager>
+{
+	friend class Manager<GameManager>;
+public:
+
+
+protected:
+	GameManager();
+	~GameManager();
+};
+
+#endif
--- TD_Frame/main.cpp
+++ TD_Frame/main.cpp
@@ -1,1 +1,7 @@
 #define SDL_MAIN_HANDLED
+#include <iostream>
+#include "game_manager.h"
+
+int main() {
+	GameManager* instance = GameManager::instance();
+}
