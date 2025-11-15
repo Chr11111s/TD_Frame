fd5b069d70478d27751016efc73f6df27d711b9d
Chr11111s <2292312771@qq.com>
2025-11-15 21:03:35 +0800

    瓦片地图渲染功能实现&定时器和程序动画实现&Vector2二维向量类实现&Enemy敌人基类实现&敌人子类实现
    瓦片地图渲染功能实现&定时器和程序动画实现&Vector2二维向量类实现&Enemy敌人基类实现&敌人子类实现

--- TD_Frame/game_manager.h
+++ TD_Frame/game_manager.h
@@ -2,6 +2,9 @@
 #define _GAME_MANAGER_H_
 
 #include "manager.h"
+#include "config_manager.h"
+#include "resources_manager.h"
+
 #include <SDL.h>
 #include <SDL_mixer.h>
 #include <SDL_ttf.h>
@@ -43,11 +46,22 @@ protected:
 
 		SDL_SetHint(SDL_HINT_IME_SHOW_UI, "1");
 
-		window = SDL_CreateWindow(u8"Tower_Defense", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 720, SDL_WINDOW_SHOWN);
+		ConfigManager* config = ConfigManager::instance();
+
+		init_assert(config->map.load("map.csv"), u8"¼ÓÔØÓÎÏ·µØÍ¼Ê§°Ü£¡");
+		init_assert(config->load_level_config("level.json"), u8"¼ÓÔØ¹Ø¿¨ÅäÖÃÊ§°Ü£¡");
+		init_assert(config->load_game_config("config.json"), u8"¼ÓÔØÓÎÏ·ÅäÖÃÊ§°Ü£¡");
+
+		window = SDL_CreateWindow(config->basic_template.window_title.c_str(), SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+			config->basic_template.window_width, config->basic_template.window_height, SDL_WINDOW_SHOWN);
 		init_assert(window, u8"´´½¨ÓÎÏ·´°¿ÚÊ§°Ü£¡");
 
 		renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC | SDL_RENDERER_TARGETTEXTURE);
 		init_assert(renderer, u8"´´½¨äÖÈ¾Æ÷Ê§°Ü£¡");
+
+		init_assert(ResourcesManager::instance()->load_from_file(renderer), u8"¼ÓÔØÓÎÏ·×ÊÔ´Ê§°Ü£¡");
+
+		init_assert(generate_tile_map_texture(), u8"Éú³ÉÍßÆ¬µØÍ¼ÎÆÀíÊ§°Ü£¡");
 	}
 	~GameManager() {
 		SDL_DestroyRenderer(renderer);
@@ -63,6 +77,8 @@ private:
 
 	SDL_Window* window = nullptr;
 	SDL_Renderer* renderer = nullptr;
+	SDL_Texture* tex_tile_map = nullptr;
+
 private:
 	void init_assert(bool flag, const char* err_msg) {
 		if (flag)return;
@@ -80,7 +96,82 @@ private:
 	}
 
 	void on_render() {
+		static ConfigManager* instance = ConfigManager::instance();
+		static SDL_Rect& rect_dst = instance->rect_tile_map;
+		SDL_RenderCopy(renderer, tex_tile_map, nullptr, &rect_dst);
+	}
 
+	bool generate_tile_map_texture() {
+		const Map& map = ConfigManager::instance()->map;
+		const TileMap& tile_map = map.get_tile_map();
+		SDL_Rect& rect_tile_map = ConfigManager::instance()->rect_tile_map;
+		SDL_Texture* tex_tile_set = ResourcesManager::instance()->get_texture_pool().find(ResID::Tex_Tileset)->second;
+
+		int width_tex_tile_set, height_tex_tile_set;
+		SDL_QueryTexture(tex_tile_set, nullptr, nullptr, &width_tex_tile_set, &height_tex_tile_set);
+		int num_tile_single_line = (int)std::ceil((double)width_tex_tile_set / SIZE_TILE);
+
+		int width_tex_tile_map, height_tex_tile_map;
+		width_tex_tile_map = (int)map.get_width() * SIZE_TILE;
+		height_tex_tile_map = (int)map.get_height() * SIZE_TILE;
+		tex_tile_map = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_ARGB8888,
+			SDL_TEXTUREACCESS_TARGET, width_tex_tile_map, height_tex_tile_map);
+		if (!tex_tile_map) return false;
+
+		ConfigManager* config = ConfigManager::instance();
+		rect_tile_map.x = (config->basic_template.window_width - width_tex_tile_map) / 2;
+		rect_tile_map.y = (config->basic_template.window_height - height_tex_tile_map) / 2;
+		rect_tile_map.w = width_tex_tile_map;
+		rect_tile_map.h = height_tex_tile_map;
+
+		SDL_SetTextureBlendMode(tex_tile_map, SDL_BLENDMODE_BLEND);
+		SDL_SetRenderTarget(renderer, tex_tile_map);
+
+		for (int y = 0; y < map.get_height(); y++)
+		{
+			for (int x = 0; x < map.get_width(); x++)
+			{
+				SDL_Rect rect_src;
+				const Tile& tile = tile_map[y][x];
+
+				const SDL_Rect& rect_dst =
+				{
+					x * SIZE_TILE, y * SIZE_TILE,
+					SIZE_TILE, SIZE_TILE
+				};
+
+				rect_src =
+				{
+					(tile.terrian % num_tile_single_line) * SIZE_TILE,
+					(tile.terrian / num_tile_single_line) * SIZE_TILE,
+					SIZE_TILE, SIZE_TILE
+				};
+				SDL_RenderCopy(renderer, tex_tile_set, &rect_src, &rect_dst);
+
+				if (tile.decoration >= 0)
+				{
+					rect_src =
+					{
+						(tile.decoration % num_tile_single_line) * SIZE_TILE,
+						(tile.decoration / num_tile_single_line) * SIZE_TILE,
+						SIZE_TILE, SIZE_TILE
+					};
+					SDL_RenderCopy(renderer, tex_tile_set, &rect_src, &rect_dst);
+				}
+			}
+		}
+
+		const SDL_Point& idx_home = map.get_idx_home();
+		const SDL_Rect rect_dst =
+		{
+			idx_home.x * SIZE_TILE, idx_home.y * SIZE_TILE,
+			SIZE_TILE, SIZE_TILE
+		};
+		SDL_RenderCopy(renderer, ResourcesManager::instance()->get_texture_pool().find(ResID::Tex_Home)->second, nullptr, &rect_dst);
+
+		SDL_SetRenderTarget(renderer, nullptr);
+
+		return true;
 	}
 };
 
